e3ba3a63aad8eeb5d8f9ccc3d9491f592312beb0
Aiden Seo <209713785+aidenjseo@users.noreply.github.com>
2025-08-12 01:00:47 -0600

    modified it so commas are considered punctuation

--- main.py
+++ main.py
@@ -31,14 +31,14 @@ def invert_line(line):
     tokens = tokenize(line)
     tokens.reverse()
 
-    if tokens and tokens[0] and tokens[0][-1] in ".!?":
+    if tokens and tokens[0] and tokens[0][-1] in ".!?,":
         tokens[0] = tokens[0][-1] + tokens[0][:-1]
     for i in range(len(tokens)):
         if tokens[i].startswith("(") and tokens[i].endswith(")"):
             inside = tokens[i][1:-1]
             inside_words = inside.split()         
             inside_words.reverse()                 
-            if inside_words and inside_words[0][-1] in ".!?":
+            if inside_words and inside_words[0][-1] in ".!?,":
                 inside_words[0] = inside_words[0][-1] + inside_words[0][:-1]
             tokens[i] = "(" + " ".join(inside_words) + ")" 
     return " ".join(tokens)
